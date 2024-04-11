2af26307adbf8b5e7e3210fa62a5aa8c37341c4c
Kim Kudsk <kim.kudsk@gmail.com>
2024-04-11 22:46:03 +0200

    Load image

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+#include <stdint.h>
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 int main() {
-  printf("Hello, World\n");
+  const char *file_path = "images/Broadway_tower_edit.jpg";
+
+  int width, height;
+  uint32_t *pixels = (uint32_t *)stbi_load(file_path, &width, &height, NULL, 4);
+  if (pixels == NULL) {
+    fprintf(stderr, "ERROR: could not read %s\n", file_path);
+    return 1;
+  }
+
+  printf("File path: %s\n", file_path);
+  printf("Width: %d\n", width);
+  printf("Height: %d\n", height);
+
   return 0;
 }
